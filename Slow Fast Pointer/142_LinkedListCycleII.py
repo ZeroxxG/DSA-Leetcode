f88c5473513498e7712f9bde508683a066428aee
ZeroxxG <mdhamd90@gmail.com>
2026-07-31 22:40:52 +0530

    Solved Linked List Cycle II

---
+++ Slow Fast Pointer/142_LinkedListCycleII.py
@@ -0,0 +1,15 @@
+slow = head
+fast = head
+
+while(fast!=None and fast.next!=None):
+    slow = slow.next
+    fast = fast.next.next
+
+    if slow == fast:
+        slow = head
+        while(slow!=fast):
+            slow = slow.next
+            fast = fast.next
+        return slow   # return fast
+
+return None
